dc7b58bfb7990c5e9854ebc2c623c324d39ad200
Daniil Kostetsky <daniilkostetsky@gmail.com>
2023-05-08 21:00:52 +0300

    add presentation layer and fragments layouts

--- app/src/main/java/com/example/calculate/presentation/MainActivity.kt
+++ app/src/main/java/com/example/calculate/presentation/MainActivity.kt
@@ -7,6 +7,6 @@ import com.example.calculate.R
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.fragment_game)
     }
 }
