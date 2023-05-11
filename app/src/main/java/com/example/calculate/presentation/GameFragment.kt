4d24ff1546846515bee7a6cf450383fb823d7228
Daniil Kostetsky <daniilkostetsky@gmail.com>
2023-05-11 17:52:02 +0300

    add viewBinding and parcelable

--- app/src/main/java/com/example/calculate/presentation/GameFragment.kt
+++ app/src/main/java/com/example/calculate/presentation/GameFragment.kt
@@ -1,20 +1,81 @@
 package com.example.calculate.presentation
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.example.calculate.R
+import com.example.calculate.databinding.FragmentGameBinding
+import com.example.calculate.domain.entities.GameLevel
+import com.example.calculate.domain.entities.GameResult
+import com.example.calculate.domain.entities.GameSettings
 
 class GameFragment : Fragment() {
+
+    private lateinit var gameLevel: GameLevel
+
+    private var _binding: FragmentGameBinding? = null
+    private val binding get() = _binding ?: throw RuntimeException("FragmentGameBinding = null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        parseArgs()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+    ): View {
+        _binding = FragmentGameBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.tvAnswerOption1.setOnClickListener {
+            launchGameFinishedFragment(
+                GameResult(
+                    true,
+                    0,
+                    0,
+                    GameSettings(0,0,0,0)
+                )
+            )
+        }
     }
 
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
+    private fun parseArgs() {
+        requireArguments().getParcelable<GameLevel>(KEY_LEVEL)?.let {
+            gameLevel = it
+        }
+    }
+
+    private fun launchGameFinishedFragment(gameResult: GameResult) {
+        requireActivity().supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, GameFinishedFragment.newInstance(gameResult))
+            .addToBackStack(null)
+            .commit()
+    }
+
+    companion object {
+
+        const val NAME = "GameFragment"
+        private const val KEY_LEVEL = "level"
+
+        fun newInstance(gameLevel: GameLevel): GameFragment {
+            return GameFragment().apply {
+                arguments = Bundle().apply {
+                    putParcelable(KEY_LEVEL, gameLevel)
+                }
+            }
+        }
+    }
 }
